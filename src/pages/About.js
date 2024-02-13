3cc3e727ec8ee69d53ae2af24b4da514c19268c9
Adityakhot <79695204+AdityaKhot@users.noreply.github.com>
2024-02-13 11:29:44 +0530

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -12,11 +12,11 @@ export default function About(){
             </div>
 
             <div className = "about-bio">
-            Hi, I’m Aditya! I’m currently a student at PES University studying Computer science and Enginerring with a minor in Business. My goal is to build efficient cloud infrasture designs and Deploy using CI/CD Pipelines, and my background AWS cloud, devops technology, and CI-CD tools for handling complete Software Deployments and Reliability.<br/><br/>
+            Hi, I’m Aditya! I’m currently a student at PES University studying Computer science and Enginerring with a minor in Business. My goal is to build efficient cloud infrasture designs and Deploying Projects using CI/CD Pipelines, and my background in AWS cloud, devops technology, and CI-CD tools for handling complete Software Deployments and Reliability.<br/><br/>
             
-            In my free time, I love drawing and watching cat videos.<br/><br/>
+            In my free time, I host knowledge transfer sessions for DevOps Enthusiasts.<br/><br/>
             
-            I am currently looking for full-time opportunities starting Summer/Fall 2024. Let’s connect! Feel free to contact me at khotaditya22@gmail.com<br/><br/>
+            I am currently looking for full-time opportunities starting Summer/Fall 2024 to Work as an Software and Cloud Engineer. Let’s connect! Connect with me at khotaditya22@gmail.com<br/><br/>
 
             <a href={resume} target = "_blank" rel="noreferrer">Resume</a>
             </div>
